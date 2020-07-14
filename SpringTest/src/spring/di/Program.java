f3d4dbf3e8c06fe8e961310c151c3c5fccb0f695
prlhspt <prlhspt@naver.com>
2020-07-14 17:50:48 +0900

    Spring

--- SpringTest/src/spring/di/Program.java
+++ SpringTest/src/spring/di/Program.java
@@ -1,16 +1,33 @@
 package spring.di;
 
-import spring.di.enity.Exam;
-import spring.di.enity.NewlecExam;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.di.entity.Exam;
 import spring.di.ui.ExamConsole;
+import spring.di.ui.GridExamConsole;
+import spring.di.ui.InlineExamConsole;
 
 public class Program {
-
+	
 	public static void main(String[] args) {
 		
+		/* 스프링에게 지시하는 방법으로 코드를 변경
 		Exam exam = new NewlecExam();
-		ExamConsole console = new ExamConsole(exam);
+		ExamConsole console = new GridExamConsole();
+		
+		console.setExam(exam);
+		*/
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
+		
+		ExamConsole console = (ExamConsole) context.getBean("console");
+		//ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
 	}
 
 }
--- SpringTest/src/spring/di/enity/Exam.java
+++
@@ -1,6 +0,0 @@
-package spring.di.enity;
-
-public interface Exam {
-	int total();
-	float avg();
-}
