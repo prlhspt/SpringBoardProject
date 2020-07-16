3aaa88be629e493cf4a1b141e33adb80d6450613
prlhspt <prlhspt@gmail.com>
2020-07-17 03:15:41 +0900

    annotation

--- SpringTest/src/spring/di/Program.java
+++ SpringTest/src/spring/di/Program.java
@@ -23,21 +23,18 @@ public class Program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
-		Exam exam = context.getBean(Exam.class);
+		//Exam exam = context.getBean(Exam.class);
 		//System.out.println(exam.toString());
 		
 		ExamConsole console = (ExamConsole) context.getBean("console"); // 이름을 통해 가져오는 방법
 		//ExamConsole console = context.getBean(ExamConsole.class); // 자료형을 통해 가져오는 방법
-		//console.print();
+		console.print();
 		
-		List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
+		//List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
 		//exams.add(new NewlecExam(1,1,1,1));
 		
 		
-		
-		for(Exam e : exams)
-			System.out.println(e);
-			
+		 	
 	}
 
 }
